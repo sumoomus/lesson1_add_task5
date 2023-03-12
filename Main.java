2e6dbf783d0b590c0f1c6a53a641e1f643f90c03
Ilya <127449810+sumoomus@users.noreply.github.com>
2023-03-12 23:45:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int q = 10;
+        int w = 3;
+        int result = q / w;
+        int remainder = q % w;
+
+        System.out.println("Result: " + result + " remainder " + remainder);
+    }
+}
